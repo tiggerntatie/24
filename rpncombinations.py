f5a75ab73cb1384f58270631a3fd909311e3545f
Eric Dennison <ericd@netdenizen.com>
2018-10-07 17:16:08 -0400

    Updated from Brython Server: 10/7/2018 5:16:07 PM

--- rpncombinations.py
+++ rpncombinations.py
@@ -1,5 +1,7 @@
 from itertools import permutations, combinations_with_replacement
 
+
+"""
 operators = ['*','/','+','-']
 
 opcombos = set()
@@ -11,8 +13,61 @@ for c in combinations_with_replacement(operators, 3): # and every combination of
 for o in opcombos:  # RPN N1 N2 O1 N3 O2 N4
     print("((x{0}x){1}x){2}x".format(*o))
 
-for o in opcombos:  # RPN
+for o in opcombos:  # RPN N1 N2 O1 N3 N4 O2 O3
     print("((x{0}x){2}(x{1}x))".format(*o))
+    
+"""    
+
+type1 = [
+    "((x*x)*x)*x",
+    "((x*x)+x)*x",
+    "((x+x)*x)*x",
+    "((x*x)-x)*x",
+    "((x/x)+x)*x",
+    "((x-x)*x)*x",
+    "((x/x)-x)*x",
+    "((x+x)+x)*x",
+    "((x+x)-x)*x",
+    "((x-x)-x)*x",
+    "((x*x)*x)/x",
+    "((x*x)/x)/x",
+    "((x*x)+x)/x",
+    "((x+x)*x)/x",
+    "((x*x)-x)/x",
+    "((x-x)*x)/x",
+    "((x/x)/x)/x",
+    "((x/x)+x)/x",
+    "((x+x)/x)/x",
+    "((x/x)-x)/x",
+    "((x-x)/x)/x",
+    "((x+x)+x)/x",
+    "((x+x)-x)/x",
+    "((x-x)-x)/x",
+    "((x*x)*x)+x",
+    "((x*x)/x)+x",
+    "((x*x)+x)+x",
+    "((x+x)*x)+x",
+    "((x-x)*x)+x",
+    "((x/x)/x)+x",
+    "((x/x)+x)+x",
+    "((x+x)/x)+x",
+    "((x-x)/x)+x",
+    "((x+x)+x)+x",
+    "((x*x)*x)-x",
+    "((x*x)/x)-x",
+    "((x*x)+x)-x",
+    "((x+x)*x)-x",
+    "((x*x)-x)-x",
+    "((x-x)*x)-x",
+    "((x/x)/x)-x",
+    "((x/x)+x)-x",
+    "((x+x)/x)-x",
+    "((x/x)-x)-x",
+    "((x-x)/x)-x",
+    "((x+x)+x)-x",
+    "((x+x)-x)-x",
+    "((x-x)-x)-x"]
+
     
 """  Culled by hand for algebraically equivalent or similar operations
 ((x*x)*x)*x
