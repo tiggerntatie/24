d8f033ebb4f10dc6e723711921043407ade0a236
Eric Dennison <ericd@netdenizen.com>
2018-10-07 17:00:08 -0400

    Updated from Brython Server: 10/7/2018 5:00:07 PM

---
+++ rpncombinations.py
@@ -0,0 +1,121 @@
+from itertools import permutations, combinations_with_replacement
+
+operators = ['*','/','+','-']
+
+opcombos = set()
+
+for c in combinations_with_replacement(operators, 3): # and every combination of operators
+    for o in permutations(c):
+        opcombos.add(o)
+
+for o in opcombos:  # RPN N1 N2 O1 N3 O2 N4
+    print("((x{0}x){1}x){2}x".format(*o))
+
+for o in opcombos:  # RPN
+    print("((x{0}x){2}(x{1}x))".format(*o))
+    
+"""  Culled by hand for algebraically equivalent or similar operations
+((x*x)*x)*x
+((x*x)*x)/x
+((x*x)*x)+x
+((x*x)+x)*x
+((x+x)*x)*x
+((x*x)*x)-x
+((x*x)-x)*x
+((x-x)*x)*x
+((x*x)/x)/x
+((x*x)/x)+x
+((x*x)+x)/x
+((x/x)+x)*x
+((x+x)*x)/x
+((x*x)/x)-x
+((x*x)-x)/x
+((x/x)-x)*x
+((x-x)*x)/x
+((x*x)+x)+x
+((x+x)*x)+x
+((x+x)+x)*x
+((x*x)+x)-x
+((x+x)*x)-x
+((x+x)-x)*x
+((x-x)*x)+x
+((x-x)+x)*x
+((x*x)-x)-x
+((x-x)*x)-x
+((x-x)-x)*x
+((x/x)/x)/x
+((x/x)/x)+x
+((x/x)+x)/x
+((x+x)/x)/x
+((x/x)/x)-x
+((x/x)-x)/x
+((x-x)/x)/x
+((x/x)+x)+x
+((x+x)/x)+x
+((x+x)+x)/x
+((x/x)+x)-x
+((x+x)/x)-x
+((x+x)-x)/x
+((x-x)/x)+x
+((x/x)-x)-x
+((x-x)/x)-x
+((x-x)-x)/x
+((x+x)+x)+x
+((x+x)+x)-x
+((x+x)-x)-x
+((x-x)-x)-x
+((x*x)/(x*x))
+((x*x)*(x/x))
+((x*x)+(x*x))
+((x*x)-(x*x))
+((x*x)/(x/x))
+((x/x)/(x*x))
+((x/x)*(x/x))
+((x*x)+(x/x))
+((x*x)/(x+x))
+((x/x)+(x*x))
+((x/x)*(x+x))
+((x+x)/(x*x))
+((x+x)*(x/x))
+((x*x)-(x/x))
+((x*x)/(x-x))
+((x/x)-(x*x))
+((x/x)*(x-x))
+((x-x)/(x*x))
+((x-x)*(x/x))
+((x+x)*(x+x))
+((x*x)-(x+x))
+((x*x)+(x-x))
+((x+x)-(x*x))
+((x+x)*(x-x))
+((x-x)+(x*x))
+((x-x)*(x+x))
+((x*x)-(x-x))
+((x-x)-(x*x))
+((x-x)*(x-x))
+((x/x)/(x/x))
+((x/x)+(x/x))
+((x/x)/(x+x))
+((x+x)/(x/x))
+((x/x)-(x/x))
+((x/x)/(x-x))
+((x-x)/(x/x))
+((x+x)/(x+x))
+((x/x)-(x+x))
+((x/x)+(x-x))
+((x+x)-(x/x))
+((x+x)/(x-x))
+((x-x)+(x/x))
+((x-x)/(x+x))
+((x/x)-(x-x))
+((x-x)-(x/x))
+((x-x)/(x-x))
+((x+x)-(x+x))
+((x+x)+(x-x))
+((x+x)-(x-x))
+((x-x)-(x+x))
+((x-x)+(x-x))
+((x-x)-(x-x))
+
+
+"""
