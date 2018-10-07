e9d534d11d07618c15fb3fd45a9935be435f3fd7
Eric Dennison <ericd@netdenizen.com>
2018-10-07 17:32:52 -0400

    Updated from Brython Server: 10/7/2018 5:32:51 PM

--- rpncombinations.py
+++ rpncombinations.py
@@ -17,7 +17,7 @@ for o in opcombos:  # RPN N1 N2 O1 N3 N4 O2 O3
     print("((x{0}x){2}(x{1}x))".format(*o))
     
 """    
-
+# Culled by hand for algebraically equivalent or similar operations
 type1 = [
     "((x*x)*x)*x",
     "((x*x)+x)*x",
@@ -68,6 +68,10 @@ type1 = [
     "((x+x)-x)-x",
     "((x-x)-x)-x"]
 
+
+
+[print("lambda a,b,c,d: ((a{0}b){1}c){2}d,".format(s[3],s[6],s[9])) for s in type1]
+
 type2 = [
     "((x*x)/(x*x))
     "((x*x)*(x/x))
@@ -115,6 +119,10 @@ type2 = [
     "((x-x)-(x+x))
     "((x-x)+(x-x))
     "((x-x)-(x-x))"]    
+
+[print("lambda a,b,c,d: ((a{0}b){1}(c{2}d),".format(s[3],s[6],s[9])) for s in type2]
+
+
     
 """  Culled by hand for algebraically equivalent or similar operations
 ((x*x)*x)*x
